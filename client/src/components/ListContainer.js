e0397951da481733aaf186bfaf136caa31e18301
Sharvy Ahmed <sharvy2008@gmail.com>
2019-06-12 23:21:43 +0600

    Added Rack CORS support. Added List adding functionality. Added Foreman.

--- client/src/components/ListContainer.js
+++ client/src/components/ListContainer.js
@@ -1,27 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+import List from "./List";
+import NewListForm from "./NewListForm";
 
 class ListContainer extends Component {
-  constructor(props) { 
+  constructor(props) {
     super(props);
     this.state = {
       lists: []
-    }
+    };
   }
 
-  componentDidMount() { 
-    axios.get('http://localhost:3001/api/v1/lists.json')
-      .then(response => { 
-        console.log(response)
-        this.setState({list: response.data})
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/api/v1/lists.json")
+      .then(response => {
+        console.log(response);
+        this.setState({ lists: response.data });
       })
-      .catch(error => console.log(error))
+      .catch(error => console.log(error));
   }
+
+  addNewList = (title, excerpt) => {
+    axios
+      .post("/api/v1/lists", { list: { title, excerpt } })
+      .then(response => {
+        console.log(response);
+        const lists = [...this.state.lists, response.data];
+        this.setState({ lists });
+      })
+      .catch(error => console.log(error));
+  };
 
   render() {
     return (
       <div className="List-container">
-        Lists
+        {this.state.lists.map(list => {
+          return <List list={list} key={list.id} />;
+        })}
+        <NewListForm onNewList={this.addNewList} />
       </div>
     );
   }
